f1ce94988d93c2efe33ca99072706cd802de86f5
soumya <soumyaprusty2481@gmail.com>
2025-06-27 23:12:38 +0530

    update bk

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,14 +21,16 @@ exports.googleAuthSuccess = (req, res) => {
   }
 
   const token = createToken(req.user);
+  const CLIENT_REDIRECT_URL =
+    process.env.CLIENT_REDIRECT_URL || "http://localhost:5173";
 
-  return res.status(200).json({ token });
+  // Send token as query param
+  return res.redirect(`${CLIENT_REDIRECT_URL}/login?token=${token}`);
 };
 
 exports.logout = (req, res) => {
   req.logout((err) => {
     if (err) return next(err);
-
     res.status(200).json({ message: "Logout successful" });
   });
 };
